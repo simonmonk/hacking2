0d938e70d94322e9445bf08ea6dc4d145c8cd8e1
simonmonk <srmonk@gmail.com>
2017-01-10 09:54:00 +0000

    rfid

---
+++ pi/ch08_rfid_read/rfid_read.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import SimpleMFRC522
+
+reader = SimpleMFRC522.SimpleMFRC522()
+
+print("Hold a tag near the reader")
+
+try:
+    while True:
+        tag = reader.read()
+        print(tag['id'])
+        print(tag['text'])
+
+finally:
+    print("cleaning up")
+    GPIO.cleanup()
